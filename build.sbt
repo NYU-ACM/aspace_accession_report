81db6817dcd57705740b22832fc7adcde30be231
Donald <don.mennerich@nyu.edu>
2017-07-31 13:48:20 -0400

    adding configuration support

--- build.sbt
+++ build.sbt
@@ -1,5 +1,9 @@
 scalaVersion := "2.12.2"
 
+assemblyJarName in assembly := "accessions_report.jar"
+
+mainClass in assembly := Some("aspace.Main")
+
 libraryDependencies ++= Seq(
   "org.json4s" % "json4s-jackson_2.12" % "3.5.0",
   "org.json4s" % "json4s-native_2.12" % "3.5.0",
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,19 +15,24 @@ object Main extends App {
 	
   implicit val formats = DefaultFormats
 
-  val conf = ConfigFactory.load("application.conf")
+  val conf = ConfigFactory.parseFile(new File("aspace.conf"))
   val header = "X-ArchivesSpace-Session"
   val url = conf.getString("aspace.url")
   println(url)
   val accUrl = s"/repositories/${conf.getString("aspace.repoId")}/accessions"
   val client = HttpClients.createDefault
-  val output = new java.io.File(conf.getString("aspace.csvFilename"))
-  val writer = new java.io.FileWriter(output)
+  val csvFile = new java.io.File(conf.getString("aspace.csvFilename"))
+  val writer = new java.io.FileWriter(csvFile)
+  writer.write("\"id1\",\"id2\",\"id3\",\"id4\",\"title\",\"accession_date\",\"extent_num\",\"extent_type\"\n")
+  writer.flush()
   val errorWriter = new java.io.FileWriter("errors.log")
-
+  var errors = 0
+  var success = 0
   val key = getKey()
   val accs = getAccessions
+	println(s"Exporting ${accs.size} accessions to ${csvFile.getName}")
   accs.foreach { accId => getAccession(accId) }
+  println(s"export completed, $success records exported successfully, $errors errors")
 
   writer.close
   errorWriter.close
@@ -48,7 +53,7 @@ object Main extends App {
   }
 
   def getAccessions(): List[Int] = {
-    println("getting accessions")
+    println("getting list of accessions")
 	  val get = new HttpGet(url + accUrl + "?all_ids=true")
 	  get.addHeader(header, key)
 	  val response = client.execute(get)
@@ -63,30 +68,33 @@ object Main extends App {
   }
 
   def getAccession(id: Int): Unit = {
-    println(s"getting: $id")
-  	val get = new HttpGet(url + accUrl + "/" + id)
-  	get.addHeader(header, key)
-	  val response = client.execute(get)
-	  val entity = response.getEntity
-	  val content = entity.getContent
-	  val data = scala.io.Source.fromInputStream(content).mkString
-	  val json = parse(data)
-	  var title_err: String = ""
-	  try {
-	  val id0 = (json \ "id_0")
-	  val id1 = (json \ "id_1")
-	  val id2 = (json \ "id_2")
-	  val id3 = (json \ "id_3")
-	  val title = (json \ "title").extract[String]
-	  title_err = title
-	  val aDate = (json \ "accession_date").extract[String]
-	  val extents = json \ "extents"
-	  val num = (extents(0) \ "number").extract[String].toDouble
-	  val extentType = (extents(0) \ "extent_type").extract[String]
-	  writer.write("\"" + getString(id0) + "\",\"" + getString(id1) + "\",\"" + getString(id2) + "\",\"" + getString(id3) + "\",\"" +  title + "\",\"" + aDate + "\",\"" + num + "\",\"" + extentType + "\"\n")
-	  writer.flush
+      val get = new HttpGet(url + accUrl + "/" + id)
+      get.addHeader(header, key)
+      val response = client.execute(get)
+      val entity = response.getEntity
+      val content = entity.getContent
+      val data = scala.io.Source.fromInputStream(content).mkString
+      val json = parse(data)
+      var title_err: String = ""
+      try {
+      val id0 = (json \ "id_0")
+      val id1 = (json \ "id_1")
+      val id2 = (json \ "id_2")
+      val id3 = (json \ "id_3")
+      val title = (json \ "title").extract[String]
+      title_err = title
+      val aDate = (json \ "accession_date").extract[String]
+      val extents = json \ "extents"
+      val num = (extents(0) \ "number").extract[String].toDouble
+      val extentType = (extents(0) \ "extent_type").extract[String]
+      writer.write("\"" + getString(id0) + "\",\"" + getString(id1) + "\",\"" + getString(id2) + "\",\"" + getString(id3) + "\",\"" +  title + "\",\"" + aDate + "\",\"" + num + "\",\"" + extentType + "\"\n")
+      writer.flush
+      success = success + 1
 	  } catch {
-	    case e: Exception => errorWriter.write(title_err)
+	    case e: Exception => {
+        errorWriter.write(s"$title_err \n")
+        errors = errors + 1
+      }
 	  }
 	  EntityUtils.consume(entity)
 	  response.close
